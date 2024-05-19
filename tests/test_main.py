5a750312daa075eac3af9362111793189aa3cd0f
kkkyosyo <kimura.k.az@m.titech.ac.jp>
2024-05-19 17:22:21 +0900

    Add opthub_evaluator and tests

---
+++ tests/test_main.py
@@ -0,0 +1,22 @@
+"""This module implements the tests for the evaluator."""
+from opthub_evaluator.main import Evaluator
+
+
+def test_evaluator() -> None:
+    """Test the evaluator."""
+    docker_image = "opthub/sphere:latest"
+    evaluator = Evaluator(docker_image, {"SPHERE_OPTIMA": "[[1, 0, 0], [0, 1, 0]]"})
+
+    result = evaluator.run([0, 0, 0])
+
+    eps = 1e-2
+
+    if abs(result["objective"][0] - 1) > eps or abs(result["objective"][1] - 1) > eps:
+        msg = f"The objective value is not correct: result is {result['objective']}, expected [1, 1]."
+        raise ValueError(msg)
+
+    result = evaluator.run([0.5, 0, 0.5])
+
+    if abs(result["objective"][0] - 0.5) > eps or abs(result["objective"][1] - 1.5) > eps:
+        msg = f"The objective value is not correct: result is {result['objective']}, expected [0.5, 1.5]."
+        raise ValueError(msg)
